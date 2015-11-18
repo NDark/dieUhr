3dcd196e3b6e0e273de41f7c70bfd57ae0d2f79c
NDark <ndarkteng@gmail.com>
2015-11-19 00:01:42 +0800

    add ClockData.cs

---
+++ UnityProject/Assets/Script/ClockData.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class ClockData
+{
+    static UILabel m_Label = null;
+    static int m_Minute = 0;
+    static int m_Hour = 0;
+    public static void SetupLabel(UILabel _Label)
+    {
+        m_Label = _Label;
+    }
+    public static void DoCalculateString(string _Key, int value)
+    {
+        if ("Minute" == _Key)
+        {
+            m_Minute = value/6;
+        }
+        else if ("Hour" == _Key)
+        {
+            m_Hour = value/30;
+        }
+        CalculateString();
+    }
+
+    static void CalculateString()
+    {
+        Debug.Log("m_Minute" + m_Minute);
+        Debug.Log("m_Hour"+ m_Hour);
+        if (null == m_Label)
+        {
+            return;
+        }
+
+        m_Label.text = "CalculateString()";
+    }
+}
--- UnityProject/Assets/Script/DragArrow.cs
+++ UnityProject/Assets/Script/DragArrow.cs
@@ -3,18 +3,26 @@ using System.Collections;
 
 public class DragArrow : MonoBehaviour
 {
-
+    public UILabel uiLabel = null;
+    public string key = "";
     bool m_IsPress = false;
     Vector3 centerVec = new Vector3(0.5f, 0.5f, 0);
     public float halfScreenWidth = 320;
     public float halfScreenHeight = 568;
     public float halfClockWidth = 320;
     public Camera UICamera = null;
+    float m_Angle = 0;
+    UISprite m_Sprite = null;
     // Use this for initialization
     void Start ()
     {
         halfScreenWidth = Screen.width / 2;
         halfScreenHeight = Screen.height / 2;
+        if (null != uiLabel)
+        {
+            ClockData.SetupLabel(uiLabel);
+        }
+        m_Sprite = this.GetComponent<UISprite>();
     }
 	
 	// Update is called once per frame
@@ -38,11 +46,12 @@ public class DragArrow : MonoBehaviour
             viewPointPosition.Normalize();
             float angle = Vector3.Angle(Vector3.up, viewPointPosition);
             Debug.Log("angle=" + angle);
-
+            
             if (x < 0)
             {
                 angle = 360 - angle;
             }
+            m_Angle = angle;
             this.transform.localRotation = Quaternion.AngleAxis(angle, -Vector3.forward);
 
         }
@@ -51,6 +60,10 @@ public class DragArrow : MonoBehaviour
     void OnPress( bool _Press )
     {
         m_IsPress = _Press;
-        
+        if (false == _Press)
+        {
+            ClockData.DoCalculateString(this.key, (int)(m_Angle));
+            
+        }
     }
 }
