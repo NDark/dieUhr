aebf5434ada58ccf4814cb0fe4fd67bee4f4a694
NDark <ndarkteng@gmail.com>
2015-11-21 18:46:16 +0800

    add Title.unity

---
+++ UnityProject/Assets/Script/ChooseLanguage.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChooseLanguage : MonoBehaviour {
+
+    public void ChooseDeutsch()
+    {
+        Localization.language = "Deutsch";
+    }
+    public void ChooseEnglish()
+    {
+        Localization.language = "English";
+    }
+    public void ChooseTraditionChinese()
+    {
+        Localization.language = "TraditionalChinese";
+    }
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
