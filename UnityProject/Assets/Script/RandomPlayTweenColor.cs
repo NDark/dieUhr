8b7ceac9bd13ac77010baa42110e2b82ada1ef31
NDark <ndarkteng@gmail.com>
2015-11-28 12:23:24 +0800

    add Publish.txt add Doc.txt add RandomPlayTweenColor.cs add unityversion.txt

---
+++ UnityProject/Assets/Script/RandomPlayTweenColor.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomPlayTweenColor : MonoBehaviour
+{
+    float nextPlay = 0.0f;
+    TweenColor color = null;
+    bool isReadyToStop = false;
+    // Use this for initialization
+    void Start ()
+    {
+        RandomNextPlay();
+        color = this.GetComponent<TweenColor>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (null == color)
+        {
+            return;
+        }
+
+        if (Time.timeSinceLevelLoad > nextPlay )
+        {
+            if (true == isReadyToStop)
+            {
+                this.enabled = false;
+                return;
+            }
+
+            color.Play();
+            RandomNextPlay();
+        }
+	
+	}
+
+    void OnPress(bool _Active)
+    {
+        isReadyToStop = true;
+
+    }
+
+    void RandomNextPlay()
+    {
+        nextPlay = Time.timeSinceLevelLoad + Random.Range(10, 30);
+    }
+}
