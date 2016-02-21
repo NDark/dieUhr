dbab201cd2afe59f7fc4b2a451316169c274bf84
NDark <ndarkteng@gmail.com>
2016-02-21 16:28:13 +0800

    move background.png to WhileSliced.png
    add label

--- UnityProject/Assets/Script/Location/LocationSystem.cs
+++ UnityProject/Assets/Script/Location/LocationSystem.cs
@@ -4,8 +4,12 @@ using System.Collections;
 public class LocationSystem : MonoBehaviour 
 {
 	public GameObject referenceObject = null ;
+	public UILabel answerLabel = null ;
 	
-	private Vector3 m_TargetPos = Vector3.zero ;
+	private int m_TargetIndex = 0 ;
+	private Vector3 [] m_TargetPositions = new Vector3[2] ;
+	private string [] m_Answers = new string[2] ;
+	
 	public bool IsInAnimation { get ; set ; }
 	
 	public float m_MovingSpeed = 0.1f ;
@@ -17,7 +21,9 @@ public class LocationSystem : MonoBehaviour
 			return ;
 		}
 		// actually we move reference up 70
-		m_TargetPos = new Vector3( 0 , 0 ) ;
+		m_TargetIndex = 0 ;
+		NGUITools.SetActive( answerLabel.gameObject , false ) ;
+		NGUITools.SetActiveChildren( answerLabel.gameObject , false ) ;
 		this.IsInAnimation = true ;
 	}
 	public void TryMoveDown()
@@ -26,14 +32,23 @@ public class LocationSystem : MonoBehaviour
 		{
 			return ;
 		}
-		m_TargetPos = new Vector3( 0 , -70 ) ;
+		m_TargetIndex = 1 ;
+		NGUITools.SetActive( answerLabel.gameObject , false ) ;
+		NGUITools.SetActiveChildren( answerLabel.gameObject , false ) ;
 		this.IsInAnimation = true ;
 	}
 	
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start () 
+	{
+		m_TargetPositions[ 0 ] = new Vector3( 0 , -70 ) ;
+		m_Answers[ 0 ] = "Bridge" ;
+		m_TargetPositions[ 1 ] = new Vector3( 0 , 0 ) ;
+		m_Answers[ 1 ] = "Roundabout" ;
+		
+		IsInAnimation = false ;
+		
 	}
 	
 	// Update is called once per frame
@@ -48,21 +63,25 @@ public class LocationSystem : MonoBehaviour
 	
 	private void UpdateReference()
 	{
-		if( null == referenceObject )
+		if( null == referenceObject || null == answerLabel )
 		{
 			return ;
 		}
 		
 		Vector3 currentPos = referenceObject.transform.position ;
-		float dist = Vector3.Distance( currentPos , m_TargetPos ) ;
+		Vector3 targetPos = m_TargetPositions [ m_TargetIndex ] ;
+		float dist = Vector3.Distance( currentPos , targetPos ) ;
 		if( dist <= 0.5f )
 		{
-			referenceObject.transform.position = m_TargetPos ;
+			referenceObject.transform.position = targetPos ;
+			answerLabel.text = m_Answers[ m_TargetIndex ] ;
+			NGUITools.SetActive( answerLabel.gameObject , true ) ;
+			NGUITools.SetActiveChildren( answerLabel.gameObject , true ) ;
 			this.IsInAnimation = false ;
 		}
 		else
 		{
-			Vector3 nextPos = Vector3.Lerp( currentPos , m_TargetPos 
+			Vector3 nextPos = Vector3.Lerp( currentPos , targetPos 
 			                               , m_MovingSpeed ) ;
 			referenceObject.transform.position = nextPos;
 		}
