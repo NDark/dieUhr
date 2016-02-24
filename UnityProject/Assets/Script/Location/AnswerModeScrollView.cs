7191fb94b74cb1744a0b64550c1ec9cc31a835e4
NDark <ndarkteng@gmail.com>
2016-02-24 20:35:29 +0800

    add scroll of answer mode fix some translation add AnswerModeScrollView.cs

---
+++ UnityProject/Assets/Script/Location/AnswerModeScrollView.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnswerModeScrollView : MonoBehaviour 
+{
+
+	public LocationSystem m_System = null ;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnPress( bool _Down ) 
+	{
+		Debug.Log("OnPress()" + _Down);
+		
+		if( true == _Down )
+		{
+			m_PressedPoint = Input.mousePosition ;
+		}
+		else if( true == m_IsPressed )
+		{
+			// release
+			Debug.Log("m_PressedPoint" + m_PressedPoint ); 
+			Debug.Log("Input.mousePosition" + Input.mousePosition ); 
+			float yDiff = Input.mousePosition.y - m_PressedPoint.y ;
+			if( null != m_System )
+			{
+				if( yDiff < 0 )
+				{
+					// try move down
+					m_System.TryMoveDown() ;
+				}
+				else if( yDiff > 0 )
+				{
+					m_System.TryMoveUp() ;
+				}
+			}
+		}
+		
+		m_IsPressed = _Down ;
+	}
+
+	private Vector3 m_PressedPoint = Vector3.zero ;	
+	
+	private bool m_IsPressed = false ;
+}
