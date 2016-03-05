fad0af54995897101e4def736324cfef4c91c071
NDark <ndarkteng@gmail.com>
2016-03-05 12:34:30 +0800

    add AcknowledgeScene.unity add TweemPingPongAfterSec.cs

---
+++ UnityProject/Assets/Script/TweemPingPongAfterSec.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TweemPingPongAfterSec : MonoBehaviour 
+{
+	TweenAlpha tween = null ;
+	float waitingSec = 5.0f ;
+	float waitingTime = 0.0f ;
+	bool isForward = true ;
+	bool isWait = false ;
+	
+	public void OnFinished()
+	{
+		if( isForward )
+		{
+			isForward = false ;
+			tween.PlayReverse() ;
+			waitingTime = Time.timeSinceLevelLoad + waitingSec ;
+		}
+		
+	}
+	
+	// Use this for initialization
+	void Start () {
+		tween = this.GetComponent<TweenAlpha>() ;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if( false == isForward 
+		&& Time.timeSinceLevelLoad > waitingTime )
+		{
+			tween.PlayForward() ;
+			isForward = true ;
+		}
+	}
+}
