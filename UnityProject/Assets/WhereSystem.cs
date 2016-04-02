8d9ba1362ae1abaec09720d629db70d5aa5a3364
NDark <ndarkteng@gmail.com>
2016-04-02 12:49:34 +0800

    add WhereSystem.cs

---
+++ UnityProject/Assets/WhereSystem.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WhereSystem : MonoBehaviour 
+{
+	public GameObject m_Fussball = null ;
+	public GameObject m_DeskObj = null ;
+
+	// Use this for initialization
+	void Start () 
+	{
+		if( null != m_Fussball && null != m_DeskObj )
+		{
+			Transform dummy = m_DeskObj.transform.FindChild("Dummy_Uber");
+			if( null != dummy )
+			{
+				m_Fussball.transform.parent = dummy.transform ;
+				m_Fussball.transform.localPosition = Vector3.zero ;
+			}
+		}
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
