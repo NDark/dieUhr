25e489446d5e9dc9a08ab008f779f42da53ff9ff
NDark <ndarkteng@gmail.com>
2016-04-02 13:02:38 +0800

    add ReleaveScene

--- UnityProject/Assets/WhereSystem.cs
+++ UnityProject/Assets/WhereSystem.cs
@@ -5,24 +5,47 @@ public class WhereSystem : MonoBehaviour
 {
 	public GameObject m_Fussball = null ;
 	public GameObject m_DeskObj = null ;
+	public Transform m_ScenesStandbyPos = null ;
+	public Transform m_ScenesDropPos = null ;
 
 	// Use this for initialization
 	void Start () 
 	{
-		if( null != m_Fussball && null != m_DeskObj )
+		SetPresentScene( m_DeskObj , m_Fussball ) ;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		ReleaveScene( m_DeskObj , m_Fussball ) ;
+	
+	}
+	
+	public void SetPresentScene( GameObject _SceneObj , GameObject _TargetObject )
+	{
+		if( null != _TargetObject && null != _SceneObj )
 		{
-			Transform dummy = m_DeskObj.transform.FindChild("Dummy_Uber");
+			_SceneObj.transform.localPosition = m_ScenesDropPos.position ;
+			_SceneObj.transform.localRotation = m_ScenesDropPos.rotation ;
+			Transform dummy = _SceneObj.transform.FindChild("Dummy_Uber");
 			if( null != dummy )
 			{
-				m_Fussball.transform.parent = dummy.transform ;
-				m_Fussball.transform.localPosition = Vector3.zero ;
+				_TargetObject.transform.parent = dummy.transform ;
+				_TargetObject.transform.localPosition = Vector3.zero ;
 			}
 		}
 		
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
+	public void ReleaveScene( GameObject _SceneObj , GameObject _TargetObject )
+	{
+		if( null != _TargetObject && null != _SceneObj )
+		{
+			_SceneObj.transform.localPosition = m_ScenesStandbyPos.position ;
+			_SceneObj.transform.localRotation = m_ScenesStandbyPos.rotation ;			
+			Rigidbody r = _SceneObj.GetComponent<Rigidbody>() ;
+			r.isKinematic = true ;
+			_TargetObject.transform.parent = this.transform ;
+		}
 	}
 }
