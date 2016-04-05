680e601db488b0943068a9ced22f41aa87b6127a
NDark <ndarkteng@gmail.com>
2016-04-05 11:40:07 +0800

    rename tv textures make sure tv textures will not be random repeatly

--- UnityProject/Assets/Script/Where/WhereRandomLoadScreen.cs
+++ UnityProject/Assets/Script/Where/WhereRandomLoadScreen.cs
@@ -3,20 +3,24 @@ using System.Collections;
 
 public class WhereRandomLoadScreen : MonoBehaviour 
 {
-	public string [] m_TextureNames = 
+	private string [] m_TextureNames = 
 	{
-		"ChristinaStürmer" ,
-		"KÄPTNPENGDIETENTAKEL" ,
+		"ChristinaSturmer" ,
+		"Kaptnpengdittentakel" ,
 		"LadyGaga"
 	} ;
 
 	private float m_NextReloadTime = 0.0f ;
-	private float m_ReloadSec = 20.0f ;
+	private float m_ReloadSec = 5.0f ;
+	private int m_PreviousIndex = 0 ;
 
+	private Renderer m_Render = null ;
+	
 	// Use this for initialization
 	void Start () 
 	{
 		m_NextReloadTime = Time.timeSinceLevelLoad + m_ReloadSec ;
+		m_Render = this.GetComponent<Renderer>() ;
 	}
 	
 	// Update is called once per frame
@@ -32,18 +36,28 @@ public class WhereRandomLoadScreen : MonoBehaviour
 	
 	private void ReloadTexture()
 	{
-		int index = Random.Range( 0 , m_TextureNames.Length ) ;
-		string filename = m_TextureNames[ index ] ;
-		Texture2D tex = Resources.Load( "Where/TelevisionScreen/" + filename ) as Texture2D ;
-		if( null != tex )
+		int index = 0 ;
+		index = Random.Range( 0 , m_TextureNames.Length ) ; ;
+		while( index == m_PreviousIndex )
 		{
-			Renderer render = this.GetComponent<Renderer>() ;
-			if( null != render && null != render.material )
-			{
-				
-				render.material.mainTexture = tex ;
-			}
+			index = Random.Range( 0 , m_TextureNames.Length ) ; ;
 		}
+		m_PreviousIndex = index ;
+		string filename = m_TextureNames[ m_PreviousIndex ] ;
+		Texture2D tex = Resources.Load( "Where/TelevisionScreen/" + filename ) as Texture2D ;
+		if( null == tex )
+		{
+			Debug.LogError("null == tex filename" + filename);
+			return ;
+		
+		}
+
+		if( null != m_Render && null != m_Render.material )
+		{
+			m_Render.material.mainTexture = tex ;
+		}
+		
+		
 	}
 }
 
