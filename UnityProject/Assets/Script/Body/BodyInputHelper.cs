4e37a37982e52059082684ea22d410c99ec45216
ndarkteng <ndarkteng@gmail.com>
2025-09-29 14:25:17 +0800

    Add Body/BodyInputHelper.cs

---
+++ UnityProject/Assets/Script/Body/BodyInputHelper.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BodyInputHelper : MonoBehaviour
+{
+	public static BodyManager s_System = null;
+	
+	// Start is called before the first frame update
+	void Start()
+    {
+		
+	}
+
+	// Update is called once per frame
+	void Update()
+    {
+		if (true == m_IsPressed)
+		{
+			m_PressedPoint = Input.mousePosition;
+			// m_System.DetectUserMouse(m_PressedPoint);
+		}
+	}
+
+	void OnPress(bool _Down)
+	{
+		if (false == _Down // up
+			&& true == m_IsPressed)
+		{
+			s_System.OnUserClick();
+		}
+		m_IsPressed = _Down;
+	}
+
+	private Vector3 m_PressedPoint = Vector3.zero;
+	private bool m_IsPressed = false;
+
+}
--- UnityProject/Assets/Script/Body/BodyManager.cs
+++ UnityProject/Assets/Script/Body/BodyManager.cs
@@ -4,15 +4,85 @@ using UnityEngine;
 
 public class BodyManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+	public UILabel m_AnswerLabel = null;
+	public UILabel m_ExampleContent = null;
+
+	public void OnUserClick()
+	{
+		Debug.LogWarning("OnUserClick");
+		/*
+		if (m_State != WhereState.WhereState_WaitInMoveMode)
+		{
+			Debug.LogError("DetectUserMouse() invalid state.");
+			return;
+		}
+
+		if (m_CurrentWhereKey == m_CurrentSelectKey)
+		{
+			PlayCorrectAnimation(true);
+			m_CorrectAnswerWaitTime = Time.timeSinceLevelLoad + m_CorrectAnswerWaitSec;
+			m_State = WhereState.WhereState_WaitCorrectAnimation;
+		}
+		//*/
+	}
+
+
+
+	// Start is called before the first frame update
+	void Start()
     {
         
     }
+
+	void Awake()
+	{
+		BodyInputHelper.s_System = this; 
+	}
 
     // Update is called once per frame
     void Update()
     {
         
     }
+
+	public void ResetAnswerContent()
+	{
+		Debug.LogError("ResetAnswerContent");
+		/*
+		if (m_State == WhereState.WhereState_WaitInAnswerMode
+		   || m_State == WhereState.WhereState_EnterAnswerMode
+		   || m_State == WhereState.WhereState_EnterTeacherMode
+		   || m_State == WhereState.WhereState_WaitInTeacherMode
+		   )
+		{
+			m_AnswerLabel.text = CreateAnswer(m_TargetKey[0], m_CurrentSceneKey, m_CurrentWhereKey, this.m_CurrentReferenceKey);
+		}
+		else if (m_State == WhereState.WhereState_WaitInMoveMode
+				|| m_State == WhereState.WhereState_EnterMoveMode)
+		{
+			m_AnswerLabel.text = CreateInstruction(m_TargetKey[0], m_CurrentSceneKey, m_CurrentWhereKey, this.m_CurrentReferenceKey);
+		}
+		//*/
+	}
+
+	public void ResetExampleContent()
+	{
+		Debug.LogError("ResetExampleContent");
+		/*
+		string exampleKey = GetExampleKey(m_CurrentWhereKey);
+		string exampleSentence = Localization.Get(exampleKey);
+		UpdateExampleContent(exampleSentence);
+		//*/
+	}
+
+	private void UpdateExampleContent(string _Content)
+	{
+		if (null == m_ExampleContent)
+		{
+			return;
+		}
+
+		m_ExampleContent.text = _Content;
+	}
+
 }
