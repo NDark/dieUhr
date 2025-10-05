5ba946d803e26ac26c9dde0ca373958194b56974
ndarkteng <ndarkteng@gmail.com>
2025-10-05 17:56:40 +0800

    add ENABLE_USE_OpenURL_iOS

---
+++ UnityProject/Assets/Script/OpenURL_iOS.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.InteropServices;
+
+public class OpenURL_iOS
+{
+#if UNITY_IOS
+        [DllImport("__Internal")]
+        private static extern void _OpenURL(string url);
+#endif
+
+	public static void OpenURL( string url )
+	{
+#if UNITY_IOS
+        _OpenURL(url);
+#endif
+	}
+}
