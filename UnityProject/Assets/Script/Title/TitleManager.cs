ea2ae313b7695118f7f8cb89e88c0e72e1dbeebf
ndarkteng <ndarkteng@gmail.com>
2025-10-05 18:36:10 +0800

    Add Title/TitleRateMeConfirmMenu.prefab

---
+++ UnityProject/Assets/Script/Title/TitleManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleManager : MonoBehaviour
+{
+	public GameObject m_TitleRateMeConfirmMenu = null ;
+
+	public void HideRateMeMenu()
+	{
+		m_TitleRateMeConfirmMenu.SetActive(false);
+	}
+
+	public void ConfirmRateMe()
+	{
+		m_TitleRateMeConfirmMenu.SetActive(false); 
+		PlayerPrefs.SetInt("PlayerPrefs_RateMe", 1);
+	}
+
+	void Awake()
+	{ 
+		++ s_AwakeCount;
+	}
+
+	// Start is called before the first frame update
+	void Start()
+    {
+        CheckShowRateMe();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	public static int s_AwakeCount = 0 ;
+	void CheckShowRateMe()
+	{ 
+		if( s_AwakeCount>=2)
+		{
+			var hasRateMe = PlayerPrefs.GetInt("PlayerPrefs_RateMe", 0);
+			if (0 == hasRateMe)
+			{
+				m_TitleRateMeConfirmMenu.SetActive(true);
+			}
+		}
+		
+	}
+}
